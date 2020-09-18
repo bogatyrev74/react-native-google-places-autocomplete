900c457aef4edf47ecc5faf06fe824da737dddab
bogatyrev74 <64581291+bogatyrev74@users.noreply.github.com>
2020-09-18 18:03:50 +0200

    Update GooglePlacesAutocomplete.js

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -21,54 +21,46 @@ import {
 const WINDOW = Dimensions.get('window');
 
 const defaultStyles = {
-  container: {
-    flex: 1,
-  },
-  textInputContainer: {
-    backgroundColor: '#C9C9CE',
-    height: 44,
-    borderTopColor: '#7e7e7e',
-    borderBottomColor: '#b5b5b5',
-    borderTopWidth: 1 / PixelRatio.get(),
-    borderBottomWidth: 1 / PixelRatio.get(),
-    flexDirection: 'row',
-  },
-  textInput: {
-    backgroundColor: '#FFFFFF',
-    height: 28,
-    borderRadius: 5,
-    paddingTop: 4.5,
-    paddingBottom: 4.5,
-    paddingLeft: 10,
-    paddingRight: 10,
-    marginTop: 7.5,
-    marginLeft: 8,
-    marginRight: 8,
-    fontSize: 15,
-    flex: 1,
-  },
-  poweredContainer: {
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-  },
-  powered: {},
-  listView: {},
-  row: {
-    padding: 13,
-    height: 44,
-    flexDirection: 'row',
-  },
-  separator: {
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: '#c8c7cc',
-  },
-  description: {},
-  loader: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    height: 20,
-  },
+											container: {
+												paddingTop: 20,
+											paddingRight: 13,
+											paddingBottom: 13,
+											paddingLeft: 13,
+												flexDirection: "column", 
+												justifyContent: "center",
+											},
+											textInputContainer: {
+												height: 44,
+												flexDirection: 'row',
+
+										  },
+										  textInput: {
+												height:45,
+												flex:1,
+												paddingLeft:13,
+												paddingRight:13,
+												backgroundColor:'#F5F5FA',
+										  },
+										  listViewContainer: {
+											flexDirection: 'row',
+											},
+										  row: {
+											paddingTop: 10,
+											paddingRight: 13,
+											paddingBottom: 25,
+											paddingLeft: 13,
+											height: 44,
+										  },
+										  separator: {
+											height: 1,
+											backgroundColor: '#c8c7cc',
+										  },
+										  description: {},
+										  loader: {
+
+											justifyContent: 'flex-end',
+											height: 20,
+										  },
 };
 
 export default class GooglePlacesAutocomplete extends Component {
@@ -816,6 +808,10 @@ export default class GooglePlacesAutocomplete extends Component {
       this.state.listViewDisplayed === true
     ) {
       return (
+      <View style={[
+            this.props.suppressDefaultStyles ? {} : defaultStyles.listViewContainer,
+            this.props.styles.listViewContainer,
+          ]}>
         <FlatList
           scrollEnabled={!this.props.disableScroll}
           style={[
@@ -838,6 +834,7 @@ export default class GooglePlacesAutocomplete extends Component {
           ListFooterComponent={this._renderPoweredLogo}
           {...this.props}
         />
+        </View>
       );
     }
 
